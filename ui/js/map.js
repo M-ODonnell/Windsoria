de2a463737ec0f8523b8244a70278fa8fd0aad8d
M-ODonnell <meghan.tara@gmail.com>
2015-05-21 21:04:08 -0400

    demo js

--- ui/js/map.js
+++ ui/js/map.js
@@ -1,48 +1,38 @@
 $(document).ready(function(){
     $("#map").hide();
-    //$("a[href='#1']").hide();
-    $("#about").hide();
 
-    $("a[href='#2']").on('click', function(){
-        $("#about").show();
-    });
-    $("#about").on('click', function(){
-        $("#about").hide();
-    });
-
-    $("a[href='#1']").on('click', function(){
+    /*$("a[href='#1']").on('click', function(){
         $("#mapImg").show();
         $("#map").hide();
         $("a[href='#1']").hide();
-    });
+    });*/
 
     // initialize map
     var map = L.map("map");
     // mock data to present different layer choices
-        var willisteadPark = L.marker([42.318694, -83.010447]).bindPopup('Willistead Park'),
+    var willisteadPark = L.marker([42.318694, -83.010447]).bindPopup('Willistead Park'),
         uAvePark = L.marker([42.321914, -83.023215]).bindPopup('University Ave. Park'),
         cadillacStPark = L.marker([42.324992, -83.001692]).bindPopup('Cadillac St. Park');
-        var green = L.layerGroup([willisteadPark, uAvePark, cadillacStPark]);
-        var viaRail = L.marker([42.325705, -83.009319]).bindPopup('Via Rail'),
+    var green = L.layerGroup([willisteadPark, uAvePark, cadillacStPark]);
+    var viaRail = L.marker([42.325705, -83.009319]).bindPopup('Via Rail'),
         greyhound = L.marker([42.317328, -83.044252]).bindPopup('Greyhound'),
         ford = L.marker([42.318661, -82.993526]).bindPopup('Ford'),
         chrysler = L.marker([42.297333, -82.986574]).bindPopup('Chrysler');
-        var go = L.layerGroup([viaRail, greyhound]),
+    var go = L.layerGroup([viaRail, greyhound]),
         labour = L.layerGroup([ford, chrysler]);
-        var overlays = {
-            "Green": green,
-            "Go": go,
-            "Labour": labour
-        };
-        L.control.layers(null, overlays).addTo(map);
+    var overlays = {
+        "Green": green,
+        "Go": go,
+        "Labour": labour
+    };
+    L.control.layers(null, overlays).addTo(map);
 
     $('td').on('click', function(){
         $("#mapImg").hide();
         $("#map").show();
-        $("a[href='#1']").show();
 
         var latLng = [];
-       
+
         switch (this.id) {
             case '1':
                 latLng = [42.332939, -83.110171];
@@ -72,13 +62,13 @@ $(document).ready(function(){
                 latLng = [42.281016, -83.006144];
                 break;
         }
-            
+
         map.setView(latLng, 14);
 
         /*var myLayer = L.tileLayer('http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18
-        });*/
+         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+         maxZoom: 18
+         });*/
         var myLayer = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day.transit/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
             attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
             subdomains: '1234',
